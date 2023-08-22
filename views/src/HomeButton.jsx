427193b1692dcd9128eef5959ec262a9ecc00a27
samnoon1971 <samnoonabrar@gmail.com>
2023-08-22 20:57:16 +0600

    ref #22 added react frontend

---
+++ views/src/HomeButton.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+function HomeButton() {
+    let history = useHistory();
+
+    function handleClick() {
+        history.push("/");
+    }
+    return (
+        <div>
+            <button className="testButton" type="button" onClick={handleClick}>
+                Test Redirect
+            </button>
+            <small>Button trys to take user to '/'</small>
+        </div>
+    );
+}
+export default HomeButton;
